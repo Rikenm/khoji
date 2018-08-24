b1f12d11baafb04381a39b04cb939d80f0d01bb6
Riken Maharjan <rikenm@bu.edu>
2018-08-24 11:39:29 -0400

    add fixes

--- src/components/ListingListTimeLine.js
+++ src/components/ListingListTimeLine.js
@@ -2,9 +2,11 @@ import React from "react"
 import queryString from  'query-string';
 import ListingList from "../containers/ListingList"
 import Navbar from "../containers/Navbar"
-
+import MenuItem from "@material-ui/core/MenuItem";
 import "../style/listitemstyle.css"
-
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
 const ListingListTimeLine = (props) => {
 
@@ -24,9 +26,32 @@ const ListingListTimeLine = (props) => {
 
            
           
-            <Navbar/> 
-
+            {/* <Navbar/>  */}
+            <FormControl required  color="secondary">
+              <InputLabel htmlFor="age-simple">Category</InputLabel>
+              <Select color="secondary"
+                
+                name="category"
+                inputProps={{
+                  name: "category",
+                  id: "category-simple"
+                }}
+                
+              >
+                
+                <MenuItem >Hello</MenuItem>
+                <MenuItem >Hello2</MenuItem>
+                
+                )
+                
             
+                
+              
+
+              </Select>
+              {/* <FormHelperText>Required</FormHelperText> */}
+            </FormControl>
+
             
            <div className="list"> 
          
